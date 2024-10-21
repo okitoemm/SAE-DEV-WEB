90b2cdaa8258460c2b2ee7005116d02ccbb1b16f
RaphaelSpencerDaGraca <oversquad04@gmail.com>
2024-10-21 13:37:06 +0200

    Ajout des accordéons

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select the hamburger menu and the nav links
-const menuToggle = document.getElementById('mobile-menu');
-const navLinks = document.getElementById('nav-links');
-
-// Add event listener to toggle the 'active' class on nav-links when hamburger is clicked
-menuToggle.addEventListener('click', function() {
-    navLinks.classList.toggle('active');
-});
